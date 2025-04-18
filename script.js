98675d839ac4480fbd287a67a8d0c2f853414900
FreitasIsac <166455554+FreitasIsac@users.noreply.github.com>
2025-04-18 12:12:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let horasInput = document.querySelector("#horas");
+let minutosInput = document.querySelector("#minutos");
+let segundosInput = document.querySelector("#segundos");
+
+
+
+setInterval (() => {
+    let date = new Date();
+    let horas = date.getHours();
+    let minutos = date.getMinutes();
+    let segundos = date.getSeconds();
+   
+    horasInput.innerHTML = horas;
+    minutosInput.innerHTML = minutos;
+    segundosInput.innerHTML = segundos;
+    
+    if(segundos <10){
+        segundosInput.innerHTML = `0${segundos}`;
+    }
+
+    if(minutos <10){
+        minutosInput.innerHTML = `0${minutos}`;
+    }
+
+    if(horas <10){
+        horasInput.innerHTML = `0${horas}`;
+    }
+
+}, 1000);
